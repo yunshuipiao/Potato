ffb7de4d8b0b748034bcf8c4230b1be23bef6ace
sunwen <sunwen@badambiz.com>
2019-07-28 20:11:58 +0800

    修改工具类

--- module_swutils/src/main/java/com/swensun/swutils/util/KotlinUtils.kt
+++ module_swutils/src/main/java/com/swensun/swutils/util/KotlinUtils.kt
@@ -8,19 +8,25 @@ import com.swensun.swutils.SwUtils
  *
  * @author sunwen
  *
- * 常用的工具类或者代码小技巧：java
+ * 常用的工具类或者代码小技巧：kotlin
  */
 fun getMetaData(metaName: String): String {
     try {
         val appInfo = SwUtils.application.packageManager
             .getApplicationInfo(SwUtils.application.packageName, PackageManager.GET_META_DATA)
-        if (appInfo?.metaData != null) {
-            return appInfo.metaData.getString(metaName) ?: ""
-        } else {
-            return ""
+        appInfo?.let {
+            return it.metaData.getString(metaName) ?: ""
         }
     } catch (e: PackageManager.NameNotFoundException) {
         e.printStackTrace()
     }
     return ""
+}
+
+fun testNull(str: String?): String {
+    if (str == null) {
+        return ""
+    } else {
+        return str
+    }
 }
