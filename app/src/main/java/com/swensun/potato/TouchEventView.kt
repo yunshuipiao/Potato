ae4843f6510dc6e87ab109fe8d354bb9de7ebb65
sunwen <sunwen@badambiz.com>
2019-08-05 10:26:50 +0800

    performClick 提示的消除

---
+++ app/src/main/java/com/swensun/potato/TouchEventView.kt
@@ -0,0 +1,26 @@
+package com.swensun.potato
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+/**
+ * @Date 2019-08-05
+ * @author sunwen
+ * @Project Potato
+ */
+class TouchEventView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> performClick()
+        }
+        return super.onTouchEvent(event)
+    }
+
+    override fun performClick(): Boolean {
+        return super.performClick()
+    }
+}
