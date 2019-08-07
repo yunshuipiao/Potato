72edc40f1e9672a24ff2beed77712b656a11956b
sunwen <sunwen@badambiz.com>
2019-08-07 12:07:02 +0800

    主模块设置最低api为23

--- app/src/main/java/com/swensun/potato/application/PotatoApplication.kt
+++ app/src/main/java/com/swensun/potato/application/PotatoApplication.kt
@@ -2,11 +2,9 @@ package com.swensun.potato.application
 
 import android.app.Application
 import android.content.Context
-import androidx.multidex.MultiDex
 
 class PotatoApplication: Application() {
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
-        MultiDex.install(this)
     }
 }
--- module_swutils/src/main/java/com/swensun/swutils/util/DeviceInfoUtils.kt
+++ module_swutils/src/main/java/com/swensun/swutils/util/DeviceInfoUtils.kt
@@ -4,12 +4,10 @@ import android.annotation.SuppressLint
 import android.app.ActivityManager
 import android.content.Context
 import android.content.Context.ACTIVITY_SERVICE
-import android.os.Build
 import android.os.Environment
 import android.text.format.Formatter
 import com.swensun.swutils.SwUtils
 import android.os.StatFs
-import java.io.File
 
 
 @SuppressLint("StaticFieldLeak")
@@ -20,31 +18,15 @@ fun getRamInfo(): String {
     val info = ActivityManager.MemoryInfo()
     manager.getMemoryInfo(info)
     val availMem = Formatter.formatFileSize(context, info.availMem)
-    val totalMem = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-        Formatter.formatFileSize(context, info.totalMem)
-    } else {
-        "0"
-    }
+    val totalMem = Formatter.formatFileSize(context, info.totalMem)
     return "$availMem / $totalMem"
 }
 
 fun getInternalMemorySizeInfo(): String {
     val path = Environment.getDataDirectory()
     val statFs = StatFs(path.path)
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-        Formatter.formatFileSize(context, statFs.availableBytes) + " / " + Formatter.formatFileSize(
-            context,
-            statFs.totalBytes
-        )
-    } else {
-        val blockSize = statFs.blockSize.toLong()
-        val totalBlocks = statFs.blockCount.toLong()
-        val totalSize = blockSize * totalBlocks
-
-        val availableBlocks = statFs.availableBlocks.toLong()
-        val availableSize = blockSize * availableBlocks
-        Formatter.formatFileSize(context, availableSize) + " / " + Formatter.formatFileSize(context, totalSize)
-    }
+    return Formatter.formatFileSize(context, statFs.availableBytes) + " / " +
+            Formatter.formatFileSize(context, statFs.totalBytes)
 }
 
 fun isSdCardExist(): Boolean {
