cea9d8905804643bf516bf841a5345df3c6bff31
yunshuipiao <1416316137@qq.com>
2019-09-04 18:22:37 +0800

    删除无关的布局资源，适配屏幕时添加

--- app/src/main/java/com/swensun/potato/LayoutFragment.kt
+++ app/src/main/java/com/swensun/potato/LayoutFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_layout.*
 
 class LayoutFragment : Fragment() {
 
@@ -29,7 +30,9 @@ class LayoutFragment : Fragment() {
     }
 
     private fun initView() {
+        btn.setOnClickListener {
 
+        }
     }
 }
 
