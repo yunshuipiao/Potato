7a122658d01eddb4e436e9ee983ebb1e81a789ae
sunwen <1416316137@qq.com>
2019-10-18 22:22:38 +0800

    修改字体

--- app/src/main/java/com/swensun/potato/LayoutFragment.kt
+++ app/src/main/java/com/swensun/potato/LayoutFragment.kt
@@ -1,11 +1,14 @@
 package com.swensun.potato
 
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.swensun.swutils.util.LogUtils
 import kotlinx.android.synthetic.main.fragment_layout.*
 
 class LayoutFragment : Fragment() {
@@ -30,8 +33,21 @@ class LayoutFragment : Fragment() {
     }
 
     private fun initView() {
-        btn.setOnClickListener {
+        setTypeFace(rootView, "eng.ttf")
+        LogUtils.d("tv size:" + tv1.textSize + " / " + tv2.textSize)
+    }
 
+
+    public fun setTypeFace(view: ViewGroup, font: String) {
+        val typeface = Typeface.createFromAsset(view.context.assets, font)
+        var count = view.childCount
+        (0 until count).forEach {
+            var v = view.getChildAt(it)
+            if (v is TextView) {
+                v.typeface = typeface
+            } else if (v is ViewGroup) {
+                setTypeFace(v, font)
+            }
         }
     }
 }
