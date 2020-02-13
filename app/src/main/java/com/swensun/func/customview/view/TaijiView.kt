2819e67aad68d721f333d64de1ad364fea24cf6f
yunshuipiao <1416316137@qq.com>
2020-02-13 23:12:36 +0800

    太极图完成

--- app/src/main/java/com/swensun/func/customview/view/TaijiView.kt
+++ app/src/main/java/com/swensun/func/customview/view/TaijiView.kt
@@ -1,6 +1,5 @@
 package com.swensun.func.customview.view
 
-import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
@@ -19,20 +18,40 @@ class TaijiView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
+    private var startAngle: Float = 0f
+    private val threeLineList = arrayListOf<List<Boolean>>().apply {
+        add(arrayListOf(false, false, false))
+        add(arrayListOf(true, false, false))
+        add(arrayListOf(true, false, true))
+        add(arrayListOf(true, true, false))
+
+        add(arrayListOf(true, true, true))
+        add(arrayListOf(false, true, true))
+        add(arrayListOf(false, true, false))
+        add(arrayListOf(false, false, true))
+    }
+    /**
+     * 1 : 顺时针
+     * -1： 逆时针
+     */
+    private var direction = 1
     val paint = Paint().apply {
 
     }
-
-
-    val anim = ObjectAnimator.ofFloat(this, "rotation", 0f, 360f)
+    
+    val anim = ValueAnimator.ofFloat(0f, 360f)
         .apply {
-            duration = 800
+            duration = 2000
             repeatCount = ValueAnimator.INFINITE
             interpolator = LinearInterpolator()
+            addUpdateListener {
+                startAngle = it.animatedValue as Float
+                invalidate()
+            }
         }
 
     val DEFAULT_SIZE = if (getWinWidth() < getWinHeight()) getWinWidth() else getWinHeight()
-    val STROKE_WIDTH = dp2px(1f).toFloat()
+    val STROKE_WIDTH = dp2px(0f).toFloat()
 
     var size: Int = 0
         get() = if (measuredWidth < measuredHeight) measuredWidth else measuredHeight
@@ -88,6 +107,7 @@ class TaijiView @JvmOverloads constructor(
         super.onDraw(canvas)
         val xCenter = (right - left) / 2f
         val yCenter = (bottom - top) / 2f
+        canvas?.rotate(startAngle * direction, xCenter, yCenter)
         paint.color = getColor(R.color.white)
         canvas?.drawCircle(xCenter, yCenter, (size - STROKE_WIDTH) / 2, paint)
 
@@ -108,9 +128,10 @@ class TaijiView @JvmOverloads constructor(
         canvas?.drawCircle(xCenter, yCenter * 1.25f, size / 32f, paint)
         paint.color = getColor(R.color.black)
         canvas?.drawCircle(xCenter, yCenter * 0.75f, size / 32f, paint)
-        (0 until 8).forEach {
-//            canvas?.rotate(it * 45f)
-            drawThreeLine(canvas, arrayListOf(false, false, false))
+        canvas?.rotate(startAngle * -2 * direction, xCenter, yCenter)
+        threeLineList.forEachIndexed { index, list ->
+            canvas?.rotate(index * 45f, xCenter, yCenter)
+            drawThreeLine(canvas, list)
         }
     }
 
@@ -121,15 +142,25 @@ class TaijiView @JvmOverloads constructor(
 
     fun drawThreeLine(
         canvas: Canvas?,
-        list: ArrayList<Boolean>
+        list: List<Boolean>
     ) {
         val firstTop = size * 0.2f - reactHeight / 2
         list.forEachIndexed { index, b ->
+            paint.color = getColor(R.color.black)
             val left = size * 0.5f - reactWidth / 2
-            val top = firstTop - (index * 2) * reactHeight
+            val top = firstTop - (index * 1.8f) * reactHeight
             val right = left + reactWidth
             val bottom = top + reactHeight
             canvas?.drawRect(left, top, right, bottom, paint)
+            if (b) {
+                // 表示地
+                paint.color = getColor(R.color.white)
+                val l = left + reactWidth * 0.4f
+                val t = top - 2
+                val r = l + reactWidth * 0.2f
+                val b = bottom + 2
+                canvas?.drawRect(l, t, r, b, paint)
+            }
         }
     }
 }
