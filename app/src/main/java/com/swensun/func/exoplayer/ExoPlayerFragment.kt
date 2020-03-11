f4ff82a53a1f6e8b893645f53f4a74ff5d2a101b
yunshuipiao <1416316137@qq.com>
2020-03-11 12:23:41 +0800

    add mp4uri

--- app/src/main/java/com/swensun/func/exoplayer/ExoPlayerFragment.kt
+++ app/src/main/java/com/swensun/func/exoplayer/ExoPlayerFragment.kt
@@ -1,18 +1,25 @@
 package com.swensun.func.exoplayer
 
+import android.net.Uri
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.swensun.potato.R
+import kotlinx.android.synthetic.main.exo_player_fragment.*
 
 class ExoPlayerFragment : Fragment() {
 
     companion object {
         fun newInstance() = ExoPlayerFragment()
     }
+
+    private val exoplayer by lazy { SimpleExoPlayer.Builder(requireContext()).build() }
 
     private lateinit var viewModel: ExoPlayerViewModel
 
@@ -26,7 +33,16 @@ class ExoPlayerFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(ExoPlayerViewModel::class.java)
-        // TODO: Use the ViewModel
+        playView.player = exoplayer
+
+        val dataSourceFactory = DefaultDataSourceFactory(context, "swennsun")
+        val mp4uri = Uri.parse("https://html5demos.com/assets/dizzy.mp4")
+        val videoSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(mp4uri)
+        exoplayer.prepare(videoSource)
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        exoplayer.release()
+    }
 }
