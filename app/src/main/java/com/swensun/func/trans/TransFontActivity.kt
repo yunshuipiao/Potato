05dc2ae52205a442939318e38abede2fc80ef39e
yunshuipiao <1416316137@qq.com>
2020-04-11 12:04:44 +0800

    字体相关

--- app/src/main/java/com/swensun/func/trans/TransFontActivity.kt
+++ app/src/main/java/com/swensun/func/trans/TransFontActivity.kt
@@ -1,6 +1,8 @@
 package com.swensun.func.trans
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.swensun.potato.R
 import com.swensun.swutils.ui.px2dp
@@ -12,8 +14,12 @@ class TransFontActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trans_font)
-        tv7.setOnClickListener {
-            toast("tv7: ${px2dp(it.height.toFloat())}")
+    }
+
+    fun onClick(v: View) {
+        (v as? TextView)?.let {
+            toast("text: ${it.height}")
         }
     }
+    
 }
