9f8ad6ad8b902f8d0df5daa66ced419b48531ff6
sunwen <1416316137@qq.com>
2020-06-22 23:49:33 +0800

    JSONObject默认是 "{}", JSONArray 默认是 "[]"

--- app/src/test/java/com/swensun/potato/GsonTest.kt
+++ app/src/test/java/com/swensun/potato/GsonTest.kt
@@ -1,12 +1,8 @@
 package com.swensun.potato
 
-import com.blankj.utilcode.util.GsonUtils
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import com.google.gson.JsonObject
 import com.swensun.http.StringTypeAdapter
 import org.json.JSONObject
-import org.junit.Assert
 import org.junit.Test
 
 
@@ -20,29 +16,21 @@ class GsonTest {
 
     @Test
     fun gsonTest() {
-        println("-- gson test -- ")
 
-        val userInfo = UserInfo().apply {
-            name = "sw"
-            like = "swsw"
-            age = 11
-        }
-        val json = gson.toJson(userInfo)
+        var json = gson.toJson(Blank())
         println(json)
-
-        var user = gson.fromJson(userJson, UserInfo::class.java)
-        println(user)
+        val jsonObject = JSONObject(json)
+        println(jsonObject.optJSONObject("inner"))
     }
 }
 
+class Blank {
+    var name = "blank"
+    val inner = InnerBlank()
 
-
-class UserInfo {
-    var name = ""
-    var like: String = ""
-    var age = 0
-
-    override fun toString(): String {
-        return "name: $name, like, $like, age: $age"
-    }
 }
+
+class InnerBlank {
+    val name = "innerBlank"
+}
+
