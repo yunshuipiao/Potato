63486b666c18629b46112f6d5c4106f17bc5b6aa
yunshuipiao <1416316137@qq.com>
2020-07-01 23:44:13 +0800

    导航栏和状态栏，明天继续研究

--- app/src/main/java/com/swensun/func/status/StatusBarFragment.kt
+++ app/src/main/java/com/swensun/func/status/StatusBarFragment.kt
@@ -1,12 +1,15 @@
 package com.swensun.func.status
 
-import androidx.lifecycle.ViewModelProviders
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.swensun.potato.R
+import kotlinx.android.synthetic.main.status_bar_fragment.*
 
 class StatusBarFragment : Fragment() {
 
@@ -15,6 +18,7 @@ class StatusBarFragment : Fragment() {
     }
 
     private lateinit var viewModel: StatusBarViewModel
+    private var mCount = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,8 +29,48 @@ class StatusBarFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(StatusBarViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this).get(StatusBarViewModel::class.java)
+        btn_system_ui_dark.setOnClickListener {
+            activity?.window?.decorView?.apply {
+                systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE
+            }
+        }
+        btn_hide_status_bar.setOnClickListener {
+            activity?.window?.decorView?.apply {
+                systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            }
+        }
+        btn_after_status_bar.setOnClickListener {
+            activity?.window?.decorView?.apply {
+                systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            }
+        }
+        btn_immersive_status_bar.setOnClickListener {
+            activity?.window?.decorView?.apply {
+                if (mCount % 2 == 0) {
+                    systemUiVisibility =
+                        View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE
+                } else {
+                    systemUiVisibility =
+                        View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                }
+                mCount += 1
+            }
+        }
+
+        btn_status_bar_trans.setOnClickListener {
+            translucent()
+        }
     }
 
+    private fun translucent() {
+        activity?.window?.apply {
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            statusBarColor = Color.TRANSPARENT
+            addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+        }
+    }
 }
