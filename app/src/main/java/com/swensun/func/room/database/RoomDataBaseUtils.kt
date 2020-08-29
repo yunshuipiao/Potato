9461ec3e70759d96a3975705ecb325d2edbfe743
yunshuipiao <1416316137@qq.com>
2020-08-29 21:00:46 +0800

    room 升级，非空字段需要指定默认值和 NOT NULL

--- app/src/main/java/com/swensun/func/room/database/RoomDataBaseUtils.kt
+++ app/src/main/java/com/swensun/func/room/database/RoomDataBaseUtils.kt
@@ -3,17 +3,27 @@ package com.swensun.func.room.database
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.swensun.swutils.SwUtils
 
 object RoomDataBaseUtils {
+    private val MIGRATIONS_1_2 = object : Migration(1, 2) {
+        override fun migrate(database: SupportSQLiteDatabase) {
+            database.execSQL(
+                "ALTER TABLE RoomEntity ADD COLUMN size INTEGER DEFAULT 0 NOT NULL"
+            )
+        }
+    }
     val INSTANCE by lazy {
         Room.databaseBuilder(SwUtils.application, RDataBase::class.java, "room.db")
+            .addMigrations(MIGRATIONS_1_2)
             .allowMainThreadQueries()
             .build()
     }
 }
 
-@Database(entities = [RoomEntity::class], version = 1, exportSchema = false)
+@Database(entities = [RoomEntity::class], version = 2, exportSchema = false)
 abstract class RDataBase : RoomDatabase() {
     abstract fun roomDao(): RoomDao
 }
--- app/src/main/java/com/swensun/func/room/database/RoomEntity.kt
+++ app/src/main/java/com/swensun/func/room/database/RoomEntity.kt
@@ -1,14 +1,15 @@
 package com.swensun.func.room.database
 
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 
 @Entity
 class RoomEntity {
     @PrimaryKey
     var id = 0
-    
+
     var title: String = ""
 
-//    var size = 0
+    var size: Int = 0
 }
