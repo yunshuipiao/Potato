8615a0fb97c946a8c4a2168f0c8237293026ad29
yunshuipiao <1416316137@qq.com>
2020-10-10 20:51:59 +0800

    [feature] 结合 MultiTypeAdapter 一起使用

--- app/src/main/java/com/swensun/func/recycler/RecyclerViewFragment.kt
+++ app/src/main/java/com/swensun/func/recycler/RecyclerViewFragment.kt
@@ -1,5 +1,6 @@
 package com.swensun.func.recycler
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.*
+import com.drakeet.multitype.ItemViewDelegate
+import com.drakeet.multitype.MultiTypeAdapter
 import com.swensun.potato.R
 import com.swensun.swutils.util.Logger
 import kotlinx.android.synthetic.main.item_recycler_view.view.*
@@ -41,48 +44,47 @@ class RecyclerViewFragment : Fragment() {
         recycler_view.layoutManager = layoutManager
 
 
-        val adapter = R2Adapter()
+        /**
+         * 普通Adapter
+         */
+//        val adapter = RAdapter()
+//        recycler_view.adapter = adapter
+//        adapter.setCountListener { position, count ->
+//            Logger.d("${adapter.currentList.map { it.count }}")
+//            adapter.notifyDataSetChanged()
+//        }
+//        adapter.submitList((100 until 103).map {
+//            RInt(it)
+//        })
+//
+//        btn_refresh.setOnClickListener {
+//            adapter.currentList.add(1, RInt(100 - adapter.itemCount))
+//            adapter.notifyDataSetChanged()
+//        }
+
+        /**
+         * MultiTypeAdapter
+         */
+        val adapter = MultiTypeAdapter()
         recycler_view.adapter = adapter
-        adapter.setCountListener { position, count ->
-            Logger.d("${adapter.currentList.map { it.count }}")
-            adapter.notifyDataSetChanged()
-        }
-        adapter.submitList((100 until 103).map {
-            RInt(it)
+        adapter.register(RViewHolderDelegate().apply {
+            setCountListener { position, count ->
+                adapter.notifyDataSetChanged()
+            }
         })
+        adapter.items = (0 until 3).map { RInt(it) }
+        adapter.notifyDataSetChanged()
 
         btn_refresh.setOnClickListener {
-            // 1
-//            val list = ArrayList(adapter.currentList)
-//            list.add(1, RInt(100 - list.size))
-//            adapter.submitList(list)
-
-            //2
-            adapter.currentList.add(1, RInt(100 - adapter.itemCount))
-            adapter.notifyItemInserted(2)
-//            adapter.notifyDataSetChanged()
+            val list = ArrayList(adapter.items)
+            list.add(1, RInt(100 - adapter.itemCount))
+            adapter.items = list
+            adapter.notifyItemInserted(1)
         }
     }
 }
 
-class RAdapter : ListAdapter<RInt, RViewHolder>(RCallback()) {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RViewHolder {
-        return RViewHolder(parent)
-    }
-
-    override fun onBindViewHolder(holder: RViewHolder, position: Int) {
-        holder.setup(getItem(position), countListener)
-    }
-
-    private var countListener: ((position: Int, count: Int) -> Unit)? = null
-    fun setCountListener(function: (position: Int, count: Int) -> Unit) {
-        this.countListener = function
-    }
-}
-
-class R2Adapter : RecyclerView.Adapter<RViewHolder>() {
-
+class RAdapter : RecyclerView.Adapter<RViewHolder>() {
 
     var currentList: ArrayList<RInt> = arrayListOf()
         set(value) {
@@ -133,19 +135,24 @@ class RViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
     }
 }
 
-
-class RCallback : DiffUtil.ItemCallback<RInt>() {
-    override fun areItemsTheSame(oldItem: RInt, newItem: RInt): Boolean {
-        return oldItem.id == newItem.id
-    }
-
-    override fun areContentsTheSame(oldItem: RInt, newItem: RInt): Boolean {
-        return oldItem.count == newItem.count
-    }
-
-}
-
 class RInt(val id: Int) {
     var count = id
+}
+
+class RViewHolderDelegate : ItemViewDelegate<RInt, RViewHolder>() {
+
+    private var countListener: ((position: Int, count: Int) -> Unit)? = null
+    fun setCountListener(function: (position: Int, count: Int) -> Unit) {
+        this.countListener = function
+    }
+
+    override fun onBindViewHolder(holder: RViewHolder, item: RInt) {
+        holder.setup(item, countListener)
+    }
+
+    override fun onCreateViewHolder(context: Context, parent: ViewGroup): RViewHolder {
+        return RViewHolder(parent)
+    }
+
 }
 
