a971623b71562d1cb3ab2b16d0275b8e2568f336
yunshuipiao <1416316137@qq.com>
2020-10-15 10:04:05 +0800

    [feature] 测试协程的阻塞事件

--- app/src/main/java/com/swensun/potato/MainViewModel.kt
+++ app/src/main/java/com/swensun/potato/MainViewModel.kt
@@ -1,19 +1,32 @@
 package com.swensun.potato
 
 import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
 import com.swensun.swutils.util.Logger
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
     fun testCoroutines() {
         (0 until 1000).forEach {
             launchIO {
-                delay(1000)
+                formatJson()
                 Logger.d("thread:${Thread.currentThread().name}, $it")
             }
         }
+
     }
+
+    fun formatJson() {
+//        Logger.d(" -- start --")
+        val startTime = System.currentTimeMillis()
+        (0 until 2000).forEach {
+            val json = Gson().toJson(Model())
+            val model = Gson().fromJson<Model>(json, Model::class.java)
+        }
+//        Logger.d("-- end ${System.currentTimeMillis() - startTime} --")
+    }
+}
+
+class Model {
+    var s1 = "s1"
+    var s2 = "s2"
 }
