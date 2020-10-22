c68fa1cb3bd55f323d620f22f6a0a8a7f8814102
yunshuipiao <1416316137@qq.com>
2020-10-22 11:30:00 +0800

    [feature] 播放本地的m3u8文件

--- app/src/main/java/com/swensun/func/exoplayer/ExoPlayerActivity.kt
+++ app/src/main/java/com/swensun/func/exoplayer/ExoPlayerActivity.kt
@@ -14,14 +14,14 @@ class ExoPlayerActivity : AppCompatActivity() {
     private var video_url =
         "https://media6.smartstudy.com/ae/07/3997/2/dest.m3u8"
 
-    private var local_video_url = PathUtils.getExternalAppDownloadPath() + "/Video/lalala/dest.m3u8"
+    private var local_video_url = PathUtils.getExternalAppDownloadPath() + "/Video/dest/dest.m3u8"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.exo_player_activity)
         video_view.setUrl(local_video_url)
         val control = StandardVideoController(this)
-        control.addDefaultControlComponent("lalala", false)
+        control.addDefaultControlComponent("dest", false)
         control.setEnableOrientation(true)
         video_view.setVideoController(control)
         video_view.start()
