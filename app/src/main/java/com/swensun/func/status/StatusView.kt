d6591bf0e815772513cc7f95703df0aa2ef6e81a
yunshuipiao <1416316137@qq.com>
2020-11-06 11:25:30 +0800

    [feature] 增加状态view的相关状态

--- app/src/main/java/com/swensun/func/status/StatusView.kt
+++ app/src/main/java/com/swensun/func/status/StatusView.kt
@@ -3,7 +3,6 @@ package com.swensun.func.status
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import com.swensun.StatusEvent
