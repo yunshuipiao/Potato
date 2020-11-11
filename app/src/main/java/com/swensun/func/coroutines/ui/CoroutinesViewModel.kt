3f256fe81261a48ea1d15732a00e676c286d2297
yunshuipiao <1416316137@qq.com>
2020-11-11 15:55:21 +0800

    [feature] update

--- app/src/main/java/com/swensun/func/coroutines/ui/CoroutinesViewModel.kt
+++ app/src/main/java/com/swensun/func/coroutines/ui/CoroutinesViewModel.kt
@@ -2,7 +2,6 @@ package com.swensun.func.coroutines.ui
 
 import com.swensun.StatusViewModel
 import com.swensun.launchIO
-import com.swensun.swutils.ui.showToast
 import com.swensun.swutils.util.Logger
 import kotlinx.coroutines.*
 
@@ -15,7 +14,7 @@ class CoroutinesViewModel : StatusViewModel() {
             job = launchIO {
                 delay(2000)
                 withContext(Dispatchers.Main) {
-                    showToast("delay toast")
+                    log("success")
                 }
             }
         } catch (e: Throwable) {
@@ -26,12 +25,13 @@ class CoroutinesViewModel : StatusViewModel() {
     fun cancel() {
         launchIO {
             job?.cancelAndJoin()
+            log("cancel")
         }
     }
 
     private fun log(msg: String) {
         job?.let {
-            Logger.d("$msg job ${it.isCompleted}, ${it.isCancelled}, ${it.isActive}")
+            Logger.d("__$msg job ${it.isCompleted}, ${it.isCancelled}, ${it.isActive}, $job")
         }
     }
 }
