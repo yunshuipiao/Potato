a5951acf0b04ee25a93a948c9686ea508f2b9a9e
yunshuipiao <1416316137@qq.com>
2020-11-17 14:35:20 +0800

    [feature] update

--- app/src/main/java/com/swensun/potato/MainViewModel.kt
+++ app/src/main/java/com/swensun/potato/MainViewModel.kt
@@ -1,8 +1,6 @@
 package com.swensun.potato
 
-import android.net.Uri
 import androidx.lifecycle.ViewModel
-import com.google.gson.Gson
 import com.swensun.launchIO
 import com.swensun.swutils.util.Logger
 
@@ -18,23 +16,6 @@ class MainViewModel : ViewModel() {
     }
 
     fun formatJson() {
-//        Logger.d(" -- start --")
-        val startTime = System.currentTimeMillis()
-        (0 until 2000).forEach {
-            val json = Gson().toJson(Model())
-            val model = Gson().fromJson<Model>(json, Model::class.java)
-        }
-//        Logger.d("-- end ${System.currentTimeMillis() - startTime} --")
     }
-
-    fun test() {
-        val str = "#EXT-X-KEY:METHOD    URI=\"Https://baidu.com\", IV=0x000000"
-        val regexURI = Regex("URI=\"(.*?)\"")
-        val s = regexURI.find(str)
-        Logger.d("u: ${s.toString()}")
-    }
-}
-
-class Model(var id: Int = 0) {
-    var count = id
+    
 }
