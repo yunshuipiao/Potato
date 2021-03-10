48ed87be3d45b2e1b5270925a91109665718bc1b
yunshuipiao <1416316137@qq.com>
2021-03-10 12:01:42 +0800

    [feature] 修改 custom view

--- app/src/main/java/com/swensun/func/customview/CustomViewFragment.kt
+++ app/src/main/java/com/swensun/func/customview/CustomViewFragment.kt
@@ -1,14 +1,12 @@
 package com.swensun.func.customview
 
 import android.os.Bundle
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.swensun.potato.R
-import kotlinx.android.synthetic.main.custom_view_fragment.*
 
 class CustomViewFragment : Fragment() {
 
@@ -28,14 +26,6 @@ class CustomViewFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(CustomViewViewModel::class.java)
-        activity?.lifecycle?.addObserver(loading_view)
-        activity?.lifecycle?.addObserver(skeleton_view)
-        activity?.lifecycle?.addObserver(google_view)
-        activity?.lifecycle?.addObserver(bagua_view)
-        activity?.lifecycle?.addObserver(fo_view)
-        activity?.lifecycle?.addObserver(taiji_view)
 
-        bubble_layout.look = Gravity.BOTTOM
-        bubble_layout.lookView(float_btn)
     }
 }
