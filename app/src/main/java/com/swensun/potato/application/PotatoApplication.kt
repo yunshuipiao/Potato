4599a533c1ac120964dad3773c9807d59261ac56
yunshuipiao <1416316137@qq.com>
2021-03-11 11:42:02 +0800

    [feature] app 前后台的切换

--- app/src/main/java/com/swensun/potato/application/PotatoApplication.kt
+++ app/src/main/java/com/swensun/potato/application/PotatoApplication.kt
@@ -8,7 +8,6 @@ import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import androidx.lifecycle.ProcessLifecycleOwner
-import androidx.lifecycle.ProcessLifecycleOwnerInitializer
 import com.dueeeke.videoplayer.player.ProgressManager
 import com.dueeeke.videoplayer.player.VideoViewConfig
 import com.dueeeke.videoplayer.player.VideoViewManager
@@ -18,7 +17,6 @@ import com.swensun.swutils.SwUtils
 import com.swensun.swutils.shareprefence.SharePreferencesManager
 import com.swensun.swutils.util.ActUtils
 import com.swensun.swutils.util.ApplicationLifecycleObserver
-import org.jetbrains.anko.alert
 
 
 class PotatoApplication : Application() {
