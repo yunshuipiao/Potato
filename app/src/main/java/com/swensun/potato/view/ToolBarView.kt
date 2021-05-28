3f16ffe73237792143d876f1b61c300af6e8c3b2
yunshuipiao <1416316137@qq.com>
2021-05-28 19:29:58 +0800

    [feature] add 自定义view 使用 viewbinding

---
+++ app/src/main/java/com/swensun/potato/view/ToolBarView.kt
@@ -0,0 +1,23 @@
+package com.swensun.potato.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import com.swensun.potato.databinding.ToolBarViewBinding
+
+/**
+ * author : zp
+ * date : 2021/5/28
+ * Potato
+ */
+class ToolBarView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    val binding = ToolBarViewBinding.inflate(LayoutInflater.from(context), this, true)
+
+    init {
+        binding.tvTitle.text = "Main"
+    }
+}
