c4414a544fe9a63b512ad3b84f7032ae27c4f067
sunwen <1416316137@qq.com>
2021-08-07 13:02:17 +0800

    init

---
+++ bohao/src/main/java/com/bohao/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.bohao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bohao.databinding.ActivityMainBinding
+import com.swensun.base.BaseActivity
+
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    override fun initView(savedInstanceState: Bundle?) {
+
+    }
+}
